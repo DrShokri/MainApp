36ed2a3cd9427849d365d248aa0144416c45f989
Mahdi-Moosavi <149239864+SeyedMahdiMoosaviDerka@users.noreply.github.com>
2024-09-17 12:34:33 +0330

    rtl added

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -3,7 +3,10 @@ import Header from "../components/admin/Header";
 
 function AdminPage() {
   return (
-    <div className="dark text-foreground bg-background w-screen h-fit min-h-screen p-8 flex flex-col">
+    <div
+      dir="rtl"
+      className="dark text-foreground bg-background w-screen h-fit min-h-screen p-8 flex flex-col"
+    >
       <Header />
       <Content />
     </div>

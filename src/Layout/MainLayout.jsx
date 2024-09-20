b4e3f3d8edc9788a4a173f877e146749ca452c7b
Mahdi-Moosavi <149239864+SeyedMahdiMoosaviDerka@users.noreply.github.com>
2024-09-20 15:37:07 +0330

    adding scroll animation

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import React from "react";
 
 // components:
 import BottomFooter from "../components/common/Footer/BottomFooter";
@@ -6,18 +7,57 @@ import FooterSvg from "../components/common/Footer/FooterSvg";
 import TopFooter from "../components/common/Footer/TopFooter";
 import Header from "../components/common/Header";
 
+// motion:
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 function MainLayout() {
+  const Section = ({ children, animationVariant }) => {
+    const controls = useAnimation();
+    const [ref, inView] = useInView({ threshold: 0.2 });
+
+    React.useEffect(() => {
+      if (inView) {
+        controls.start("visible");
+      } else {
+        controls.start("hidden");
+      }
+    }, [controls, inView]);
+
+    return (
+      <motion.div
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={animationVariant}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+      >
+        {children}
+      </motion.div>
+    );
+  };
+
+  const animationVariants = {
+    hidden: { opacity: 0, y: 100 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <>
-      <Header />
+      <Section animationVariant={animationVariants}>
+        <Header />
+      </Section>
 
       <main className="">
         <Outlet />
       </main>
 
       <footer className="flex flex-col">
-        <FooterSvg />
-        <TopFooter />
+        <Section animationVariant={animationVariants}>
+          <FooterSvg />
+          <TopFooter />
+        </Section>
+
         <BottomFooter />
       </footer>
     </>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,15 +1,57 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+// components
 import Achievements from "../components/Landing/achievements";
 import HeroSection from "../components/landing/HeroSection";
 import VideoSection from "../components/landing/VideoSection";
 
-function Landing() {
+const Section = ({ children, animationVariant }) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({ threshold: 0.2 });
+
+  React.useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
   return (
-    <>
-      <HeroSection />
-      <VideoSection />
-      <Achievements />
-    </>
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      animate={controls}
+      variants={animationVariant}
+      transition={{ duration: 0.8, ease: "easeInOut" }}
+    >
+      {children}
+    </motion.div>
   );
-}
+};
+
+const animationVariants = {
+  hidden: { opacity: 0, y: 100 },
+  visible: { opacity: 1, y: 0 },
+};
+
+const Landing = () => {
+  return (
+    <div>
+      <Section animationVariant={animationVariants}>
+        <HeroSection />
+      </Section>
+      <Section animationVariant={animationVariants}>
+        <VideoSection />
+      </Section>
+      <Section animationVariant={animationVariants}>
+        <Achievements />
+      </Section>
+    </div>
+  );
+};
 
 export default Landing;
